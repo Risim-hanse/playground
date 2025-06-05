8606a0f186aedc90c270552754bda0529e9c6b19
Risim <risim-hanse@eclipso.me>
2025-06-05 06:43:52 +0530

    Update day 1.py
    Clever solution, N² -> N. x+y=2020, y=2020-x, therefore if y exists in original set then, it satisfies x+y=2020, intersection will give pair x,y that satisfies the equation since that set is unordered I split it in half through inequality to only get 1 that satisfies the equation multiply with it's conjugate ;)

--- aoc-solutions/aoc-2020/day 1.py
+++ aoc-solutions/aoc-2020/day 1.py
@@ -1,1 +1,14 @@
+from os import PathLike
+from typing import List, Set
 
+def read_puzzle(name: PathLike) -> Set[int]:
+  with open(name, r) as fp:
+    content = fp.readlines()
+    data = set(int(x) for x in content)
+  return data
+
+def part1_process(data: Set[int]) -> List[int]:
+  conjugate = set(2020-x for x in data)
+  remainder = data & conjugate
+  result = [(2020-x)* x for x in remainder if 1010 <= x]
+  return result
